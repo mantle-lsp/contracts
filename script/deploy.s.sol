3571adfd06314609a6720a1366c9df87cfe284b0
Will Demaine <willdemaine@gmail.com>
2023-10-23 14:38:55 +0800

    Add commands for deployment files

--- script/deploy.s.sol
+++ script/deploy.s.sol
@@ -9,6 +9,16 @@ import {
     deployAll, grantAndRenounceAllRoles, grantAllAdminRoles, Deployments, DeploymentParams
 } from "./helpers/Proxy.sol";
 
+import {TimelockController} from "openzeppelin/governance/TimelockController.sol";
+import {Pauser} from "../src/Pauser.sol";
+import {Oracle} from "../src/Oracle.sol";
+import {OracleQuorumManager} from "../src/OracleQuorumManager.sol";
+import {ReturnsReceiver} from "../src/ReturnsReceiver.sol";
+import {ReturnsAggregator} from "../src/ReturnsAggregator.sol";
+import {UnstakeRequestsManager} from "../src/UnstakeRequestsManager.sol";
+import {Staking} from "../src/Staking.sol";
+import {METH} from "../src/METH.sol";
+
 contract Deploy is Base {
     function _readDeploymentParamsFromEnv() internal view returns (DeploymentParams memory) {
         address[] memory reporters = vm.envAddress("REPORTER_ADDRESSES", ",");
@@ -68,5 +78,26 @@ contract Deploy is Base {
         vm.startBroadcast();
         grantAllAdminRoles(ds, newAdmin);
         vm.stopBroadcast();
+    }
+
+    function logDeploymentsFile() public {
+        Deployments memory deps = readDeployments();
+        logDeployments(deps);
+    }
+
+    function writeManualDeploymentsFile() public {
+        Deployments memory dep = Deployments({
+            proxyAdmin: TimelockController(payable(0x00)),
+            mETH: METH(0x00),
+            oracle: Oracle(0x00),
+            quorumManager: OracleQuorumManager(0x00),
+            pauser: Pauser(0x00),
+            aggregator: ReturnsAggregator(payable(0x00)),
+            consensusLayerReceiver: ReturnsReceiver(payable(0x00)),
+            executionLayerReceiver: ReturnsReceiver(payable(0x00)),
+            staking: Staking(payable(0x00)),
+            unstakeRequestsManager: UnstakeRequestsManager(payable(0x00))
+        });
+        writeDeployments(dep);
     }
 }
